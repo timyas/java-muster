bc40a67a73f15981d397b34d3b90864c552e9be6
timyas <max@maxims-air.fritz.box>
2016-01-25 20:57:16 +0100

    Update of Tracke_Issues.sql

--- model-project/TRACKER_ISSUES.sql
+++ model-project/TRACKER_ISSUES.sql
@@ -2,9 +2,16 @@
 
 create table issues(id_issues int not null primary key,
 issue_name VARCHAR(45) NULL,
-Ddescription VARCHAR(254) NULL,
+description VARCHAR(254) NULL,
 date VARCHAR(45) NULL,
 author VARCHAR(45) NULL);
+
+-- add information to issues--
+
+insert into issues values(1, 'First Issue',
+ 'This is an issue about my first expierience',
+ '25-01-16',
+ 'Maxim');
 
 -- Create table comments--
 
@@ -13,14 +20,27 @@ author VARCHAR(45) NULL,
 date VARCHAR(45) NOT NULL,
 comments VARCHAR(254) NULL);
 
+-- add information to comments--
+
+insert into comments values(1, 'John Smith', '26-01-16',
+'Some information about comment');
+
 -- Create table users--
 
 create table users(id_user int not null primary key,
 admins varchar(45) null,
 users varchar(45) null);
 
+-- add information to users--
+
+insert into users values(1, null, 'John Smith');
+insert into users values(2, 'Maxim', null);
+
 -- Create table Files --
 
 create table files(id_file int not null primary key,
 file varchar(45) null);
 
+-- add information to files--
+
+insert into files values (1, 'a file');
