f071680adb9181a6e3197bf0de9ff1b516a9e836
timyas <max@maxims-air.fritz.box>
2016-02-07 17:52:52 +0100

    Updated toString, equals and hashCode methods

--- model-project/src/main/java/com/solovey/Role.java
+++ model-project/src/main/java/com/solovey/Role.java
@@ -1,4 +1,5 @@
 package com.solovey;
+
 public class Role {
 	private int id;
 	
@@ -26,8 +27,22 @@ public class Role {
 	public String getName(){
 		return name;
 	}
-	
+	@Override
 	public String toString(){
-		return id+" "+name;
+		return String.format("%2d --> %s", id, name);
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		if(!(obj instanceof Role)) return false;
+		Role role = (Role)obj;	
+		return this.id == role.id && this.name.equals(role.name);
+	}
+	@Override
+	public int hashCode(){
+		int result = 17;
+		result = 31 * result + id;
+		result = 31 * result + name.hashCode();
+		return result;
 	}
 }
